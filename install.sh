ffe11d47a1a1c9090b06ecc8a095b17663f49503
Paul <krepa098@gmail.com>
2019-08-08 17:24:28 +0200

    install.sh: flags

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="/usr"
+cmake .. -DCMAKE_INSTALL_PREFIX="/usr" -DBUILD_GENERATOR_GNOME=ON -DBUILD_GENERATOR_KDE=ON
 cmake --build . --config Release
 sudo make install
 kbuildsycoca5
